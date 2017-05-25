30ad6b5446443b878c388ef1449ca9c8cde01013
Petar Prodanovic <petar.prodanovic@gmail.com>
2017-05-25 23:30:44 +0200

    sredjena klasa za upis redova nabavke.

--- unos_nabavke.php
+++ unos_nabavke.php
@@ -6,30 +6,37 @@ $prd = new products();
 $allprd = $prd->getAllProducts();
 $countprd = count($allprd);
 
-if(isset($_POST["saveDellivery"]) != ''){
-    $suplierid = $_POST["selectSupplier"];
-    $date = new DateTime();
-    $formatDate = $date->format("Y-m-d");
-    if ($suplierid > 0) {
-        $ord = new orders();
-        $ord->addOrder($formatDate,$suplierid,$session->userid);
-        $maxOrder = $ord->getMaxId();
-        print($maxOrder->id);
-        unset($ord,$suplierid);
+
+if (isset($_POST["saveDellivery"]) != ''){
+$suplierid = $_POST["selectSupplier"];
+$date = new DateTime();
+$formatDate = $date->format("Y-m-d");
+if ($suplierid > 0) {
+    $ord = new orders();
+    $ord->addOrder($formatDate, $suplierid, $session->userid);
+    $maxOrder = $ord->getMaxId();
+    $allproductsOrder = array();
+    $currentOrderId = $maxOrder->id;
+
 //        header("Location:unos_nabavke.php");
+
+    for ($j = 1; $j <= $countprd; $j++) {
+        $item = $_POST['item' . $j];
+        $value = $_POST['quantity' . $j];
+        $price = $_POST['order_price' . $j];
+        if ($value > 0 and $price > 0) {
+            $tmpSupplie = new supplies($value, $currentOrderId,$item,$price);
+            $tmpSupplie->addSupplie();
+        }
+
+
     }
 
 //    echo $suplierid;
 //    header("Location:unos_nabavke.php");
-//    for ($j=1; $j <= $countprd; $j++){
-//        $item = $_POST['item'.$j];
-//        $value = $_POST['quantity'.$j];
-//        $price = $_POST['order_price'.$j];
-//        if($value >0 and $price >0) {
-//            echo "$item - $price ($value)";
-//        }
+
 //
-//    }
+    }
 
 
 }
@@ -47,13 +54,16 @@ include $menuLayout;
 
             <select id="selectSupplier" name="selectSupplier" required>
                 <option></option>
-                <?php $sup = new suppliers(); $allsuppliers = $sup->getAllSuppliers(); foreach ($allsuppliers as $item) {
+                <?php $sup = new suppliers();
+                $allsuppliers = $sup->getAllSuppliers();
+                foreach ($allsuppliers as $item) {
                     ?>
                     <option
                         value="<?php echo $item->id ?>"><?php echo $item->name ?></option>
 
                     <?php
-                } unset ($sup, $allsuppliers)?>
+                }
+                unset ($sup, $allsuppliers) ?>
             </select>
 
             <div class="login-fields">
@@ -73,8 +83,10 @@ include $menuLayout;
                         <div class="order">
                             <div class="item"><?php echo $item->productname ?></div>
                             <input type="hidden" name="item<?php echo $i ?>" id="item<?php echo $i ?>" value="<?php echo $item->id ?>">
-                            <input type="number" id="quantity<?php echo $i ?>" name="quantity<?php echo $i ?>" placeholder="kol" class="quantity" onchange="calculateSum(<?php echo $i ?>)"/>
-                            <input type="number" step="0.01" id="order_price<?php echo $i ?>" name="order_price<?php echo $i ?>" placeholder="cena" class="order_price" onchange="calculateSum(<?php echo $i ?>)"/>
+                            <input type="number" id="quantity<?php echo $i ?>" name="quantity<?php echo $i ?>" placeholder="kol" class="quantity"
+                                   onchange="calculateSum(<?php echo $i ?>)"/>
+                            <input type="number" step="0.01" id="order_price<?php echo $i ?>" name="order_price<?php echo $i ?>" placeholder="cena" class="order_price"
+                                   onchange="calculateSum(<?php echo $i ?>)"/>
                             <div class="full_price" id="full_price<?php echo $i ?>"></div>
                         </div> <!-- /field -->
                     </div> <!-- /field -->
