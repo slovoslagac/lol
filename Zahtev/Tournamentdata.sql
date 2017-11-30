bac6399b2fb24175db26d836a07a3214a3bc36e0
Petar Prodanovic <petar.prodanovic@gmail.com>
2017-11-30 23:45:40 +0100

    no message

--- Zahtev/Tournamentdata.sql
+++ Zahtev/Tournamentdata.sql
@@ -1,2 +1,5 @@
 insert into games (name) value ('FIFA 18');
 
+insert into platforms (name) values ('PS4'), ('PC');
+
+insert into tournaments (name, type, tournamentgame, platform, tournamenttime) values ('Fifa 18 - Mesečni turnri - Decembar (1/15)', 1, 1,1 , '2017-12-03 10:00:00')
